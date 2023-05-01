cf3dec81517a9c2dfc50c28e41b0d1ff86a1b763
IrynaYrrr <78929283+IrynaYrrr@users.noreply.github.com>
2023-05-01 14:13:19 +0000

    Deploying to gh-pages from @ IrynaYrrr/goit-js-hw-09@f6fdb5d62b938b78439234ee59f18c3ad73692cb 🚀

---
+++ 01-color-switcher.19bf7a45.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let d=null;t.addEventListener("click",(function(){d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled",""),e.removeAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(d),t.removeAttribute("disabled",""),e.setAttribute("disabled","")})),e.setAttribute("disabled","");
+//# sourceMappingURL=01-color-switcher.19bf7a45.js.map
