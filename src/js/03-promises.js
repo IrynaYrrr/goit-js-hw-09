c659608de533e9ec3921f4af939b2b66c8c96aa9
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-05-02 15:48:22 +0300

    complete task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,35 @@
-const inputDelay = document.querySelector('[name = "delay"]');
+import Notiflix from 'notiflix';
+const formSubmit = document.querySelector('form');
 
-console.log(inputDelay);
-
+formSubmit.addEventListener('submit', handleSubmit);
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+
+  const delay = Number(evt.currentTarget.elements.delay.value);
+  const step = Number(evt.currentTarget.elements.step.value);
+  const amount = Number(evt.currentTarget.elements.amount.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay + step * i)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  };
+};
