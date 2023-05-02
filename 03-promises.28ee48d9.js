883e50539da4b83ca9222609cc43c2cd42e13471
IrynaYrrr <78929283+IrynaYrrr@users.noreply.github.com>
2023-05-02 10:46:08 +0000

    Deploying to gh-pages from @ IrynaYrrr/goit-js-hw-09@499757a44e7a060b32d73e3169de96ef6c28ca25 🚀

---
+++ 03-promises.28ee48d9.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector('[name = "delay"]');console.log(e)}();
+//# sourceMappingURL=03-promises.28ee48d9.js.map
---
+++ 03-promises.f020ffd0.js
@@ -0,0 +1,2 @@
+const e=document.querySelector('[name = "delay"]');console.log(e);
+//# sourceMappingURL=03-promises.f020ffd0.js.map
